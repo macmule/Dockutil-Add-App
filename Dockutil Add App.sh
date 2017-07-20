ce175b2011522647f059df29caf4d85c09afc9f3
rderewianko <rderewianko@users.noreply.github.com>
2017-07-20 09:32:30 -0700

    Update Dockutil Add App.sh
    Updating to use the @frogor method for finding the user.

--- Dockutil Add App.sh
+++ Dockutil Add App.sh
@@ -10,7 +10,7 @@
 ###
 # Get the Username of the logged in user
 ###
-loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
+loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
 echo "$loggedInUser..."
 
